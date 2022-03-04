99be169bc87cd8c19968f800eadf57529cb0da25
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-04 16:50:16 -0700

    got the first question to work
    but.... I don't think I am using console.table as intended

--- server.js
+++ server.js
@@ -6,4 +6,44 @@ const inquirer = require("inquirer");
 db.connect((err) => {
   if (err) throw err;
   console.log("Database connected.");
+  promptQuestion();
 });
+
+//The question that starts it off and serves as the hub
+const promptQuestion = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "Do something.",
+      name: "choice",
+      choices: ["View All Departments", "All Done"],
+    })
+    .then((answer) => {
+      if (answer.choice === "View All Departments") {
+        viewAllDepartments();
+      } else if (answer.choice === "All Done") {
+        console.log("You did it!!!");
+        process.exit();
+      }
+    });
+};
+
+const viewAllDepartments = () => {
+  console.log("");
+  console.log("*******************************");
+  console.log("Welcome to the department land.");
+  console.log("");
+  console.log("*******************************");
+
+  const sql = `SELECT department.id, department.name AS department_names FROM department`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    console.table(rows);
+
+    promptQuestion();
+  });
+};
