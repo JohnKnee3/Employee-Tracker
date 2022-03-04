14c8ca1473cfaaed66ee2f69ea4aeb536d8de480
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-04 14:00:46 -0700

    view all departments backend tested

--- db/query.sql
+++ db/query.sql
@@ -10,21 +10,25 @@
                 -- LEFT JOIN role 
                 -- ON employee.role_id = role.id;
 
-                SELECT employee.first_name, 
-                       employee.last_name, 
-                       role.title AS role, 
-                       role.salary  
-                FROM employee 
-                LEFT JOIN role 
-                ON employee.role_id = role.id;
+                -- SELECT employee.first_name, 
+                --        employee.last_name, 
+                --        role.title AS role, 
+                --        role.salary  
+                -- FROM employee 
+                -- LEFT JOIN role 
+                -- ON employee.role_id = role.id;
 
-                SELECT employee.first_name, 
-                       employee.last_name, 
-                       role.title AS role, 
-                       role.salary, 
-                       department.name AS department  
-                FROM employee 
-                LEFT JOIN role ON employee.role_id = role.id
-                LEFT JOIN department ON role.department_id = department.id;
+                -- SELECT employee.first_name, 
+                --        employee.last_name, 
+                --        role.title AS role, 
+                --        role.salary, 
+                --        department.name AS department  
+                -- FROM employee 
+                -- LEFT JOIN role ON employee.role_id = role.id
+                -- LEFT JOIN department ON role.department_id = department.id;
+
+                --VIEW ALL DEPARTMENTS
+                SELECT department.id, department.name AS Department_Names FROM department;
+
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -9,20 +9,21 @@ INSERT INTO role
   (title, salary, department_id)
 VALUES
   ('BIG BOSS', 14.25, 1),
-  ('small boss', 14.24, 1),
-  ('Boss of Themselves', 14.23, 2);
+  ('small boss', 14.24, 3),
+  ('Lone Wolf', 14.23, 1),
+  ('Worker Bee', 14.22, 2);
 
 
 INSERT INTO employee
   (first_name, last_name, role_id, manager_id)
 VALUES
   ('Ronald', 'Firbank', 1, 1),
-  ('Virginia', 'Woolf', 1, 1),
-  ('Piers', 'Gaveston', 1, 1),
+  ('Virginia', 'Woolf', 2, 1),
+  ('Piers', 'Gaveston', 2, 1),
   ('Charles', 'LeRoi', 2, 1),
   ('Katherine', 'Mansfield', 2, 1),
   ('Dora', 'Carrington', 3, 1),
-  ('Edward', 'Bellamy', 3, 1),
-  ('Montague', 'Summers', 3, 1),
-  ('Octavia', 'Butler', 3, 1),
-  ('Unica', 'Zurn', 2, 1);
+  ('Edward', 'Bellamy', 4, 1),
+  ('Montague', 'Summers', 4, 1),
+  ('Octavia', 'Butler', 4, 1),
+  ('Unica', 'Zurn', 4, 1);
