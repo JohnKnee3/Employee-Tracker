615615e3f31d71c55b94e1f904a69064abae2957
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-04 11:11:18 -0700

    added data to employee in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, 1),
+  ('Virginia', 'Woolf', 1, 1),
+  ('Piers', 'Gaveston', 1, 1),
+  ('Charles', 'LeRoi', 2, 1),
+  ('Katherine', 'Mansfield', 2, 1),
+  ('Dora', 'Carrington', 3, 1),
+  ('Edward', 'Bellamy', 3, 1),
+  ('Montague', 'Summers', 3, 1),
+  ('Octavia', 'Butler', 3, 1),
+  ('Unica', 'Zurn', 2, 1);
