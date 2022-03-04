dedb9ceda087a33d6ccbbc0825b8f0edfa2af93d
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-04 16:09:32 -0700

    update role basic syntax in

--- db/query.sql
+++ db/query.sql
@@ -55,8 +55,10 @@
                 -- INSERT INTO role (title, salary, department_id)
                 -- VALUES ('Mindless Robot', 14.07, 4);
 
-                INSERT INTO employee (first_name, last_name, role_id, manager_id)
-                VALUES ('Robo', 'Boto', 5, 1);
+                -- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                -- VALUES ('Robo', 'Boto', 5, 1);
+
+                -- UPDATE employee SET role_id = 3 WHERE id = 6;
 
 
 
